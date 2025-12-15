13b3048e3d66bf38c0e38d30cbc691fffb7dbeb5
Max Abramov <gobpo2002@gmail.com>
2025-12-15 16:43:54 +0600

    First task

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+)
+
+const (
+	portKey = "TODO_PORT"
+	webDir  = "web"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	port := os.Getenv(portKey)
+	if port == "" {
+		port = "7540"
+	}
+
+	fileServer := http.FileServer(http.Dir(webDir))
+	r.Handle("/", fileServer)
+
+	fmt.Println("Сервер запущен на http://localhost:" + port)
+	if err := http.ListenAndServe(":"+port, r); err != nil {
+		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
+		return
+	}
+}
